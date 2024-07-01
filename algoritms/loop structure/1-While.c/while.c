139d0be47ae860cf77b98778c47d6641b806d84c
Tiago Cezar <tiogrodev@gmail.com>
2024-07-01 13:52:04 +0000

    wiki algortims code

---
+++ algoritms/loop structure/1-While.c/while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+						int main ()
+						{ 
+	//variables
+						int oneval, twoval;
+	//limits of contain
+						int lowerlimit, upperlimit, steplimit;
+
+	/*limits of variables*/			lowerlimit=0;
+						upperlimit=300;
+						steplimit=20;
+					
+
+	/*start of contain number one*/		oneval=lowerlimit;
+	/*test*/				while(oneval<=upperlimit){
+	/*start of contain number two*/     	twoval=5*(oneval-32)/9;
+	/*function print*/ 			printf("%d\t%d\n", oneval, twoval);
+	/*rithm of contain*/			oneval= oneval+steplimit;
+							}
+						}
