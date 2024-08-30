459fa8ecad82d68e81b9ef88742d49868435b785
Tiago Cezar <tiogrodev@gmail.com>
2024-08-30 17:30:40 +0000

    Cursos

---
+++ courses/slackjeff/1-Iniciantes no terminal Linux/Módulo 1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+//Aula 1- Localização e Listas
+
+pwd // Print Work Directory
+
+ls // list
+-d // list of directory
+
+--color // Listar Argumentos
+
+//Aula 2- Navegação
+
+cd //change directory
+cd  //Arguments cd:
+
+/ //
+.. // Directory up
+~ // directory home user
+- //Return ultimo directorio acess
+
+ cd /home/courses
+
+//Aula 3- Copiando Arquivos
+
+cp - copy
+
+//Argumento cp <filename>
+
+cp -r <filename> /directory/<new nameme>
+
+cp -r <filename> 
+
+//Aula 4 - Remove files, directorys and directorys empty 
+
+rm // remove
+
+rmdir // remove directory
+
+// arguments rm
+
+-r recursive
+-v verbose
+-i interactive
+-f force
+
