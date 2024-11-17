8d23ee75599fb04ac54dc1fce7052db922e0375e
Tiago Cezar <tiogrodev@gmail.com>
2024-11-17 08:26:50 +0000

    Iniciante no Terminal - Modulo 1 - conceitos e comando básicos em Shell

--- courses/Laboratório Slackjeff/1-Iniciantes no terminal Linux/Módulo 1.sh
+++ courses/Laboratório Slackjeff/1-Iniciantes no terminal Linux/Módulo 1.sh
@@ -113,9 +113,113 @@ grep -E or egrep `[[:space:]]+` <directory/directory>
 
 #Aula 11 - Comando cut
 
+cut #cortar
+
+cut -c -d '' -f '' <filename> #Argumentos -c :seleciona e imprimi recorrências conforme solicitado.  -d: delimiter -f: field
 
 
+#Aula 12 - Comando sed
+
+sed -e  s// #
+
+sed -i  s// #
+
+sed -e  //d #
+
+sed
+
+#Aula 13 - Nano
 
 
+#Aula 14 - Vim
+
+
+#Aula 15 - Su
+
+
+#Aula 16 - Login com Variaveis de ambiente e Trocar senha de usuário
+
+su
+
+passwd <username>
+
+#Aula 17 - Atalhos bash
+
+#limpar tela
+clear ou ctrl + l
+
+#entrar ou sair do terminal
+exit ou ctrl + d
+
+#mover cursor para o inicio da linha: 
+ctrl  + a
+#mover cursor para o fim da linha:
+ctrl + e
+
+
+#loop 
+while :; do echo "xxx" ;  done 
+
+
+#pesquisar rapida
+ctrl + r <argumento da pesquisa>
+
+#historico de comandos
+$ history
+
+$!<numero da linha> 
+
+#interromper
+ctrl + c #funciona no loop e no $htop
+
+#auto completar 
+tab
+
+#Aula 21 - Gerenciador de Pacotes
+
+1 gerenciador de  baixo nivel
+	Resolver a parte burocratica do pacote: Instalar o pacote, Atualizar, remover e criar o pacote
+	dpkg rpm pkgtools
+2 Gerenciador de auto nivel
+	Resolver dependencias.
+	Procura no repositorio
+	Atualiza a lista de Repositorio
+	apt, slackpkg, yum|dnf
+
+gerenciador e proprio da distro do linux
+
+#comandos que baixam programas oficiais
+
+#Aula Gerenciador de Pacote apt
+
+Debian: Debian, ubuntu, mint, elementary, deepin
+
+#arquivo de intalacao 
+
+cd /etc/apt/sources.list #repositorio de instalacao
+
+apt update #atualiza repostorio de pacotes
+
+apt search <packagename> #procura pacote no repositorio
+
+apt remove <packagename> #remove pacote
+
+apt upgrade <packagename> #atualiza pacote 
+
+#aula de Gerenciador de Pacote dnf
+Redhat: fedora, centOS.
+
+dnf 
+vim /etc/dnf #arquivo de instalacao
+
+dnf updateapt update #atualiza repostorio de pacotes
+
+dnf search <packagename> #procura pacote no repositorio
+
+dnf  remove <packagename> #remove pacote
+
+dnf  upgrade <packagename> #atualiza pacote 
+ 
+dnf info  #informacao do pacote
 
 
